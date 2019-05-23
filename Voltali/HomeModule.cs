bc6135b2c70b5a6b66c48bfbc8048aa59972a99d
Milkey Tan <819774989@qq.com>
2019-05-23 16:21:27 +0800

    添加项目文件。

---
+++ Voltali/HomeModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace Voltali
+{
+    public sealed class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", x => "Voltali Download Manager");
+        }
+    }
+}
---
+++ Voltali/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Nancy.Hosting.Self;
+
+namespace Voltali
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Voltali Download Manager");
+            Console.WriteLine("-------------------");
+
+            using (NancyHost host = new NancyHost(new HostConfiguration
+                    {RewriteLocalhost = true, UrlReservations = new UrlReservations {CreateAutomatically = true}},
+                new Uri("http://localhost:2020/")))
+            {
+                host.Start();
+
+                Console.WriteLine("Voltali Download Manager is running on 2020 port");
+                Console.WriteLine("Press any [Enter] to close the host.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
